7eef3c554b62bcd0eba85e070db1b608295eae68
RishiDhanwantari <dhanwanr@mcmaster.ca>
2025-04-04 21:56:46 -0400

    Refactored abstract Runner class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandRunner.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandRunner.java
@@ -1,54 +1,18 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 /**
- * Runner class to represent the Maze Runner and traverse through the maze.
+ * RightHandRunner class that implements Runner with Right-Hand-Rule logic (follow right wall).
  */
 
 class RightHandRunner extends Runner {
-    private int[] runnerPosition;
-    private Direction runnerDirection;
-    private Path path;
-    private MovementHandler movementChain;
 
-    //constructor method for Runner.
+    //constructor method for RightHandRunner.
     public RightHandRunner(int[] entryPosition, Direction startingDirection){
-        this.runnerPosition = entryPosition;
-        this.runnerDirection = startingDirection;
-        this.path = new Path();
-        buildMovementChain();
-    }
-
-    //method that traverses the maze by following the right wall until the exit.
-    public void traverseMaze(Maze maze){
-        int[] exitPoint = maze.getExitPoint();
-
-        while (!(this.runnerPosition[0] == exitPoint[0] && this.runnerPosition[1] == exitPoint[1])){
-            this.movementChain.handle(this, maze);
-        }
-    }
-
-    //method that moves the Runner one position forward, whichever direction they are facing.
-    public void moveForward(){
-        int[] currentDirection = runnerDirection.getCurrentDirection();
-        this.runnerPosition[0] += currentDirection[0];
-        this.runnerPosition[1] += currentDirection[1];
-        this.path.addMovement("F");
-    }
-
-    //method that adjusts the Runner's direction by turning it right once.
-    public void runnerTurnRight(){
-        this.runnerDirection = this.runnerDirection.turnRight();
-        this.path.addMovement("R");
-    }
-
-    //method that adjusts the Runner's direction by turning it left once.
-    public void runnerTurnLeft(){
-        this.runnerDirection = this.runnerDirection.turnLeft();
-        this.path.addMovement("L");
+        super(entryPosition, startingDirection);
     }
 
     //builds handling chain
-    private void buildMovementChain() {
+    public void buildMovementChain() {
         MovementHandler right = new RightHandRightTurnHandler();
         MovementHandler forward = new ForwardMoveHandler();
         MovementHandler left = new RightHandLeftTurnHandler();
@@ -56,23 +20,5 @@ class RightHandRunner extends Runner {
         right.setSuccessor(forward);
         forward.setSuccessor(left);
         this.movementChain = right;
-    }
-
-    //accessor method for the Runner's path.
-    public Path getPath(){
-        Path copy = new Path();
-        copy.addMovement(this.path.getCanonicalPath());
-        return copy;
-    }
-
-    //accessor method for the Runner's direction.
-    public Direction getDirection(){
-        Direction directionCopy = this.runnerDirection;
-        return directionCopy;
-    }
-
-    //accessor method for the Runner's position;
-    public int[] getPosition(){
-        return new int[]{runnerPosition[0], runnerPosition[1]};
     }
 }
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Runner.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Runner.java
@@ -1,21 +1,69 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 /**
- * Abstract factory class for Runner creation.
+ * Abstract Runner class that provides a template for every Runner that traverses a Maze.
  */
 
 abstract class Runner {
-    abstract void traverseMaze(Maze maze);
+    protected int[] runnerPosition;
+    protected Direction runnerDirection;
+    protected Path path;
+    protected MovementHandler movementChain;
 
-    abstract void moveForward();
+    //constructor method for Runner.
+    public Runner(int[] entryPosition, Direction startingDirection){
+        this.runnerPosition = entryPosition;
+        this.runnerDirection = startingDirection;
+        this.path = new Path();
+        buildMovementChain();
+    }
 
-    abstract void runnerTurnRight();
+    //method that traverses the maze by following the right wall until the exit.
+    public void traverseMaze(Maze maze){
+        int[] exitPoint = maze.getExitPoint();
 
-    abstract void runnerTurnLeft();
+        while (!(this.runnerPosition[0] == exitPoint[0] && this.runnerPosition[1] == exitPoint[1])){
+            this.movementChain.handle(this, maze);
+        }
+    }
 
-    abstract Path getPath();
+    //method that moves the Runner one position forward, whichever direction they are facing.
+    public void moveForward(){
+        int[] currentDirection = runnerDirection.getCurrentDirection();
+        this.runnerPosition[0] += currentDirection[0];
+        this.runnerPosition[1] += currentDirection[1];
+        this.path.addMovement("F");
+    }
 
-    abstract Direction getDirection();
+    //method that adjusts the Runner's direction by turning it right once.
+    public void runnerTurnRight(){
+        this.runnerDirection = this.runnerDirection.turnRight();
+        this.path.addMovement("R");
+    }
 
-    abstract int[] getPosition();
+    //method that adjusts the Runner's direction by turning it left once.
+    public void runnerTurnLeft(){
+        this.runnerDirection = this.runnerDirection.turnLeft();
+        this.path.addMovement("L");
+    }
+
+    abstract void buildMovementChain();
+
+    //accessor method for the Runner's path.
+    public Path getPath(){
+        Path copy = new Path();
+        copy.addMovement(this.path.getCanonicalPath());
+        return copy;
+    }
+
+    //accessor method for the Runner's direction.
+    public Direction getDirection(){
+        Direction directionCopy = this.runnerDirection;
+        return directionCopy;
+    }
+
+    //accessor method for the Runner's position;
+    public int[] getPosition(){
+        return new int[]{this.runnerPosition[0], this.runnerPosition[1]};
+    }
 }
